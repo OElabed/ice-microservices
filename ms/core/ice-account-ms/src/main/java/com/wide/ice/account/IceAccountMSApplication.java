f9d02eaeebc0ad3b72566524ef045e39f7780d1d
oelabed <oel3abed@gmail.com>
2016-10-25 20:44:50 +0200

    add eureka to ice-account-ms

--- ms/core/ice-account-ms/src/main/java/com/wide/ice/account/IceAccountMSApplication.java
+++ ms/core/ice-account-ms/src/main/java/com/wide/ice/account/IceAccountMSApplication.java
@@ -26,8 +26,10 @@ package com.wide.ice.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class IceAccountMSApplication {
 
     public static void main(String[] args) {
