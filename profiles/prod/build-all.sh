11acaea6b29829a95ce254aeeb64f94dbb604678
oelabed <Oussama.El.Abed@fircosoft.com>
2016-11-03 18:12:52 +0100

    intials commits

--- profiles/prod/build-all.sh
+++ profiles/prod/build-all.sh
@@ -9,18 +9,26 @@ function note() {
 
 set -e
 
-# Backing microservices build
-cd ../../ms/backing/ice-auth-ms;               note "Building auth-ms..."; ./gradlew clean build; cd -
-cd ../../ms/backing/ice-config-ms;             note "Building config-ms..."; ./gradlew clean build; cd -
-cd ../../ms/backing/ice-discovery-ms;          note "Building discovery-ms..."; ./gradlew clean build; cd -
-cd ../../ms/backing/ice-getway-ms;             note "Building getway-ms..."; ./gradlew clean build; cd -
-cd ../../ms/backing/ice-hystrix-dashboard-ms;  note "Building hystrix-dashboard-ms..."; ./gradlew clean build; cd -
+# Common things build
+cd ../../ms/core/ice-common;                   note "Building common..."; ./gradlew clean build; cd -
 
 # Core microservices build
-cd ../../ms/core/ice-common;                   note "Building common..."; ./gradlew clean build; cd -
 cd ../../ms/core/ice-account-ms;               note "Building account-ms..."; ./gradlew clean build; cd -
 
-# Composite microservices build
+# Business microservices build
+
+# Customer microservices build
+cd ../../ms/customer/ice-admin-backend-ms;     note "Building admin-backend-ms..."; ./gradlew clean build; cd -
 
 # FrontEnd apps build
-cd ../../ms/frontend/ice-store-ui/scripts;     note "Building store-ui..."; ./build.sh; cd -
+cd ../../ms/gui/ice-store-ui/scripts;     note "Building store-ui..."; ./build.sh; cd -
+
+# Support microservices build
+cd ../../ms/support/ice-auth-ms;               note "Building auth-ms..."; ./gradlew clean build; cd -
+cd ../../ms/support/ice-config-ms;             note "Building config-ms..."; ./gradlew clean build; cd -
+cd ../../ms/support/ice-discovery-ms;          note "Building discovery-ms..."; ./gradlew clean build; cd -
+cd ../../ms/support/ice-getway-ms;             note "Building getway-ms..."; ./gradlew clean build; cd -
+cd ../../ms/support/ice-hystrix-dashboard-ms;  note "Building hystrix-dashboard-ms..."; ./gradlew clean build; cd -
+
+# Tools microservices build
+cd ../../ms/tools/ice-admin-ms;               note "Building admin-ms..."; ./gradlew clean build; cd -
